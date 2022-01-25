d39d963e69529c536eb0c72abaecad65d19a6b82
Antonio Martínez <antonio_martinez88@hotmail.com>
2022-01-24 20:31:13 -0400

    error in routes

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,33 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/register', methods=['POST'])
+def create_new_user():
+    body = request.get_json()
+
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if "email" not in body:
+        raise APIException('You need to specify the email', status_code=400)
+    if "password" not in body:
+        raise APIException('You need to specify the password', status_code=400)
+    if "is_active" not in body:
+        raise APIException('You need to specify the is_active', status_code=400)
+    
+    hashed_password = bcrypt.generate_password_hash(body["password"])
+
+    new_user = User(email=body['email'], password=hashed_password, is_active=body['is_active'])
+    
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"mensaje": "Usuario creado exitosamente"}), 201
+
+@api.route('/users', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    users = list(map(lambda user: user.serialize(), users))
+    return jsonify(users), 200
+
+
